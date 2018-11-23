32c60d3b2337351375c778ba1ff737aa8a2e6622
658146603 <a1533144693@163.com>
2018-11-23 23:32:20 +0800

    //

--- src/com/qscftyjm/clander/MainActivity.java
+++ src/com/qscftyjm/clander/MainActivity.java
@@ -2,12 +2,48 @@ package com.qscftyjm.clander;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+		
+		
+	}
+	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		switch(id){
+			case R.id.main_addItem:
+				Log.i("Clander","ADD");
+				break;
+			case R.id.main_refreshItem:
+				Log.i("Clander","REFRESH");
+				break;
+			case R.id.main_searchItem:
+				Log.i("Clander","SEARCH");
+				break;
+		}
+		
+		return super.onOptionsItemSelected(item);
 	}
 }
